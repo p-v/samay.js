19df1a9b0de988165e9469af454d9c1a419bd4d9
Pratyush <pratyushverma@outlook.com>
2017-06-06 16:03:18 +0530

    Add parseText method
    - Added `parseText` method which returns an object
    - Deprecated `parse` method

--- src/samay.js
+++ src/samay.js
@@ -8,10 +8,19 @@ class Samay {
     this.config = config || Config.getDefault();
   }
 
+  /*
+   * @deprecated since version 1.1
+   */
   parse(text) {
     const parsedInfo = Parser.parse(text, this.config);
     const result = Needle.stitch(parsedInfo, this.config);
     return result;
+  }
+
+  parseText(text) {
+    const parsedInfo = Parser.parse(text, this.config);
+    const result = Needle.stitch(parsedInfo, this.config);
+    return { date: result };
   }
 
 }
